fc38815f12f405e9cfd9b9db438e65f06638f000
guang.long <gwong.long.2013@gmail.com>
2013-11-24 21:54:18 -0500

    init setup p4

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>p4.monkeyaround.biz</title>
+
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />	
+
+    <!-- Common CSS/JSS -->
+    <link rel="stylesheet" href="/css/p4.css" type="text/css">
+    <link rel="stylesheet" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css" /> 
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script> 
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
+
+  </head>
+
+  <body>	
+      <h1> Welcome to p4.monkeyaround.biz, presented to you by <a href="http://p1.monkeyaround.biz">Guang Long</a>
+    <script type="text/javascript" src="/js/p4.js"></script>
+
+  </body>
+
+</html>
